18e084d6388360de024adaf565c51f30656693fa
Raul Grant <vqrgashernandezrauldejesus@gmail.com>
2025-09-03 15:11:14 -0600

    cambios Raul De Jesus Vargas Hernandez

--- task5.js
+++ task5.js
@@ -1,5 +1,25 @@
 // Refer to Task 5 in your Instructions to complete this task
 
-for (let i = 0; i < 1; i++) {
-    console.log("This is Task Five!");
-  };
+
+const prompt = require("prompt-sync")();
+
+const numLines = parseInt(prompt("¿Cuántas líneas quieres generar?"));
+
+for (let i = 1; i <= numLines; i++) {
+  let output = "";
+  if (i % 3 === 0) {
+    output += "Fizz";
+  }
+  if (i % 5 === 0) {
+    output += "Buzz";
+  }
+  if (i % 7 === 0) {
+    output += "Woof";
+  }
+
+  if (output === "") {
+    console.log(i);
+  } else {
+    console.log(output);
+  }
+}
--- task6.js
+++ task6.js
@@ -1,5 +1,25 @@
 // Refer to Task 6 in your Instructions to complete this task
 
-for (let i = 0; i < 1; i++) {
-    console.log("This is Task Six!");
-  };
+
+const result = [];
+
+for (let i = 1; i <= 105; i++) {
+  let output = "";
+  if (i % 3 === 0) {
+    output += "Fizz";
+  }
+  if (i % 5 === 0) {
+    output += "Buzz";
+  }
+  if (i % 7 === 0) {
+    output += "Woof";
+  }
+
+  if (output === "") {
+    result.push(i);
+  } else {
+    result.push(output);
+  }
+}
+
+console.log(result);
